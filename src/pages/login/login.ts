ea810d971b19a2641a5a00d07ebca8fca0e8db50
Khoi Tran <knt36@drexel.edu>
2017-02-05 15:29:35 -0500

    More

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-
+import {LoginService} from 'services/login.service';
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
