1b8f78b8fd89e186b74a6a7f19a78afe2ea000b2
Khoi Tran <knt36@drexel.edu>
2017-02-07 12:00:13 -0500

    Changed server test file to json

--- SERVER/test.php
+++ SERVER/test.php
@@ -1,3 +1,10 @@
 <?php
-	echo "hello";
+	echo "{
+  'data': [
+    { 'id': 1, 'name': 'Windstorm' },
+    { 'id': 2, 'name': 'Bombasto' },
+    { 'id': 3, 'name': 'Magneta' },
+    { 'id': 4, 'name': 'Tornado' }
+  ]
+}";
 ?>
