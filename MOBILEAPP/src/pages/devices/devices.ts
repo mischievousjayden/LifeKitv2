be5e991a0fa50f9cb21c61d97adedab33fc04ee1
Khoi Tran <knt36@drexel.edu>
2017-03-06 16:50:22 -0500

    App on device page can detect near by devices and print them to screen.

--- MOBILEAPP/src/pages/devices/devices.ts
+++ MOBILEAPP/src/pages/devices/devices.ts
@@ -6,12 +6,32 @@ import { Platform } from 'ionic-angular';
     templateUrl: 'devices.html'
 })
 export class Devices {
-
+  public discoveredBluetoothDevices: any [];
     constructor(public platform: Platform) {
 		if(this.platform.is('android')){
-			alert("I am android!");
+			alert("I am android!")
 			//Call get list of connectable devices
-			console.log(BluetoothSerial.discoverUnpaired());
+      BluetoothSerial.isEnabled().then(res => this.bluetoothOn()).catch(res => this.bluetoothOff());
+
+
+
 		}
+    }
+
+    public bluetoothOn(){
+      alert('Bluetooth is on...');
+      this.discoverUnpairedDevices();
+    }
+
+    public discoverUnpairedDevices(){
+      BluetoothSerial.discoverUnpaired().then(devices =>{
+          this.discoveredBluetoothDevices = devices;
+          this.discoverUnpairedDevices();
+      });
+    }
+    public bluetoothOff(){
+      alert('Bluetooth is off...');
+      //perform trying to turn it on.
+      BluetoothSerial.enable().then(res=>this.bluetoothOn()).catch(res=>this.bluetoothOff());
     }
 }
