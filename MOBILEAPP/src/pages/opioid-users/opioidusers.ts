0396437ca929ffbf9ffd62e088b1fd25e56b0d11
Anh Le <aql25@drexel.edu>
2017-03-12 00:29:12 -0500

    UX: npm update and add chart.js

--- MOBILEAPP/src/pages/opioid-users/opioidusers.ts
+++ MOBILEAPP/src/pages/opioid-users/opioidusers.ts
@@ -1,29 +1,64 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {CarrierSettingsModel} from '../shared/models/carrier-settings/carrier-settings.model';
-import {BluetoothSerial} from "ionic-native";
-import {Devices} from "../devices/devices";
+
 import {BluetoothService} from "../../shared/services/bluetooth.service";
 
+import { Chart } from "chart.js";
+
 @Component({
+    selector: 'opioid-users',
     templateUrl: 'opioidusers.html'
 })
+
 export class OpioidUsers {
-bluetoothData = "";
-    carrierSetting = {
-        onDuty: false,
-        hasNaloxone: false
-    };
+  bluetoothData = "";
+
+  @ViewChild('lineCanvas') lineCanvas;
+
+  lineChart: any;
+
+  carrierSetting = {
+    onDuty: false,
+    hasNaloxone: false
+  };
 
 
-    constructor(public navCtrl:NavController) {
-      //this.startBluetoothService();
-      BluetoothService.bluetoothData.subscribe(data=>{
-        document.getElementById('hello').innerHTML = data.toString();
+  constructor(public navCtrl:NavController) {
+    //this.startBluetoothService();
+    BluetoothService.bluetoothData.subscribe(data=>{
+      updateChart(data);
+    });
+  }
+  open(url){
+    this.navCtrl.push(url);
+  }
+
+  updateChart(data) {
+      this.lineChart.data.datasets[0].data[4] = data.respirPulse;
+      this.lineChart.update();
+  }
+
+  ionViewDidLoad() {
+      this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+          type: 'line',
+          data: {
+              datasets: [{
+                  label: 'Respiratory Rate',
+                  data: [0,0,0,0]
+              }]
+          },
+          options: {
+              scales: {
+                xAxes: [{
+                  type: 'linear',
+                  position: 'bottom'
+                }]
+              }
+          }
+
       });
-    }
-    open(url){
-      this.navCtrl.push(url);
-    }
+
+  }
 }
 
