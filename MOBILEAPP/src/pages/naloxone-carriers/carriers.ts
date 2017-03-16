986d49c47266f6c23abad5c1b05e9841a8f20609
Khoi Tran <knt36@drexel.edu>
2017-03-16 01:48:23 -0400

    Fixed displaying of currenty duties

--- MOBILEAPP/src/pages/naloxone-carriers/carriers.ts
+++ MOBILEAPP/src/pages/naloxone-carriers/carriers.ts
@@ -4,6 +4,8 @@ import {CarrierSettingsModel} from '../shared/models/carrier-settings/carrier-se
 import {Geolocation} from "ionic-native";
 import {EmergenecyService} from "../../shared/services/emergency.service";
 import {Emergency} from "../../shared/models/emergency.model";
+import {ReplaySubject, Observable} from "rxjs";
+import {TypeUser} from "../type-user/typeuser";
 
 
 @Component({
@@ -13,6 +15,17 @@ import {Emergency} from "../../shared/models/emergency.model";
 
 export class Carriers {
   @Input() emergencies:Array<Emergency> = new Array<Emergency>();
+  @Input() onDutyToggled: boolean;
+
+  public static sendingLocationInterval = 7000;
+  public static ondutyInterval = 5000;
+  public onDutyToggledObserver:ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
+  public sendLocationOb:Observable<any>;
+  public pageReportDutyOb:Observable<any>;
+  public unsubSendLocation:Array<any> = new Array<any>();
+  public unsubPageReport:Array<any> = new Array<any>();
+
+
   carrierSetting = {
       onDuty: true,
       hasNaloxone: true
@@ -23,8 +36,66 @@ export class Carriers {
 
   }
 
+
+  sendLocation(){
+    Geolocation.getCurrentPosition().then(resp=>{
+      console.log("reporting location");
+      this.emergencyService.updateCarrierLocation(resp.coords.latitude,resp.coords.longitude).subscribe(res=>{
+        console.log(res);
+      });
+    });
+  }
+
+  pageReportOnDuty(){
+    Geolocation.getCurrentPosition().then(resp=>{
+      console.log('reporting for duty');
+      this.emergencyService.reportOnDuty(resp.coords.latitude,resp.coords.longitude).subscribe(res=>{
+        console.log(res);
+        this.emergencies = res;
+      });
+    });
+  }
+
+  ngDoCheck(){
+    if(this.onDutyToggled){
+      //run run code for carrier actions
+      if(this.unsubSendLocation.length==0) {
+        console.log('subscribing...');
+        this.sendLocationOb = Observable.timer(0, Carriers.sendingLocationInterval);
+        this.unsubSendLocation.push(this.sendLocationOb.subscribe(t => {
+          this.sendLocation();
+        }));
+      }
+      if(this.unsubPageReport.length==0) {
+        console.log('subscribing...');
+        this.pageReportDutyOb = Observable.timer(0, Carriers.ondutyInterval);
+        this.unsubPageReport.push(this.pageReportDutyOb.subscribe(t => {
+          this.pageReportOnDuty();
+        }));
+      }
+    }else{
+      //run stop code for any carrier actions
+      if(this.unsubSendLocation.length>0){
+        console.log('unsubscribe from sending location');
+        this.unsubSendLocation.forEach(function(res){
+          res.unsubscribe();
+        });
+        this.unsubSendLocation = new Array();
+      }
+      if(this.unsubPageReport.length>0){
+        console.log('unsubscribe from duty');
+        this.unsubPageReport.forEach(function(res){
+          res.unsubscribe();
+        });
+        this.unsubPageReport = new Array();
+      }
+    }
+  }
+
   open(url){
     this.navCtrl.push(url);
   }
+
+
 
 }
--- MOBILEAPP/src/pages/type-user/typeuser.ts
+++ MOBILEAPP/src/pages/type-user/typeuser.ts
@@ -11,72 +11,13 @@ import {Emergency} from "../../shared/models/emergency.model";
 
 export class TypeUser {
   @ViewChild(Segment) segment: Segment;
-
-  public static sendingLocationInterval = 7000;
-  public static ondutyInterval = 5000;
   public onDutyToggled = false;
-  public onDutyToggledObserver:ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
-  public setIntervalID;
-  public sendLocationOb:Observable<any>;
-  public pageReportDutyOb:Observable<any>;
-
-  public unsubSendLocation;
-  public unsubPageReport;
-
-  public emergencies:Array<Emergency> = new Array();
 
   constructor(public emergencyService:EmergenecyService, public geo:Geolocation) {
   }
 
-  ngOnInit() {
-    this.onDutyToggledObserver.subscribe(res=>{
-      if(res==true){
-        console.log('subscribing...');
-        this.sendLocationOb= Observable.timer(0,TypeUser.sendingLocationInterval);
-        this.unsubSendLocation = this.sendLocationOb.subscribe(t=>{
-          this.sendLocation();
-        });
-        this.pageReportDutyOb= Observable.timer(0,TypeUser.ondutyInterval);
-        this.unsubPageReport = this.pageReportDutyOb.subscribe(t=>{
-          this.pageReportOnDuty();
-        });
-      }else{
-        console.log('suppose to unsubscribe');
-        if(this.unsubSendLocation){
-          console.log('unsubscribe from sending location');
-          this.unsubSendLocation.unsubscribe();
-        }
-        if(this.unsubPageReport){
-          console.log('unsubscribe from duty');
-          this.unsubPageReport.unsubscribe();
-        }
-      }
-    });
-  }
-
-  sendLocation(){
-    Geolocation.getCurrentPosition().then(resp=>{
-      console.log("reporting location");
-      this.emergencyService.updateCarrierLocation(resp.coords.latitude,resp.coords.longitude).subscribe(res=>{
-        console.log(res);
-      });
-    });
-  }
-
-  pageReportOnDuty(){
-    Geolocation.getCurrentPosition().then(resp=>{
-      console.log('reporting for duty');
-      this.emergencyService.reportOnDuty(resp.coords.latitude,resp.coords.longitude).subscribe(res=>{
-        console.log(res);
-        this.emergencies = res;
-      });
-    });
-  }
-
-  // toDo: implement to send data to server
   notifyOnDuty() {
     console.log(this.onDutyToggled);
-    this.onDutyToggledObserver.next(this.onDutyToggled);
   }
 }
 
